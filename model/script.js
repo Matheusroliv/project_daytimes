176e0db44a3b28d0357108bab683c79b4bb1b464
Matheusroliv <matheusrdeoliv1@gmail.com>
2020-11-25 17:52:00 -0300

    finished

---
+++ model/script.js
@@ -0,0 +1,27 @@
+function carregar() {
+
+  let msg = window.document.getElementById("msg")
+  let img = window.document.getElementById("imagem")
+  let data = new Date()
+  let hora = data.getHours()
+  msg.innerHTML = "Agora são "+ hora +" horas."
+
+    if (hora >= 5 && hora < 18) {
+
+        img.src = "/model/images/manha.png"
+        document.body.style.background = "#e2cd9f"
+    
+    }else if (hora >= 14 && hora <= 18) {
+
+        img.src = "/model/images/tarde.png"
+        document.body.style.background = "#b9846f"
+
+    } else {
+
+         img.src = "/model/images/noite.png"
+         document.body.style.background = "#515154"
+
+    }
+
+}
+
